1cb25ac4c58771cc7e7528c79edb1015bfdc5ab8
JWDonovan <jameswilliamdonovan@gmail.com>
2020-12-12 21:47:19 -0500

    Added rubocop and fixed initial linting issues

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,22 +1,24 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "is not valid without an email" do
+  it 'is not valid without an email' do
     user = User.new(email: nil)
     expect(user).to_not be_valid
   end
 
-  it "is not valid without a password" do
+  it 'is not valid without a password' do
     user = User.new(encrypted_password: nil)
     expect(user).to_not be_valid
   end
 
-  it "is not valid without a first name" do
+  it 'is not valid without a first name' do
     user = User.new(first_name: nil)
     expect(user).to_not be_valid
   end
 
-  it "is not valid without a last name" do
+  it 'is not valid without a last name' do
     user = User.new(last_name: nil)
     expect(user).to_not be_valid
   end
